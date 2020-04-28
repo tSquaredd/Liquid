e0dc166d1c480896936a88a595139805dab90bc8
Tyler Turnbull <tylerturnbull13@gmail.com>
2020-04-28 08:32:38 -0400

    LIQ-17 : Setup basic navigation with bottom nav bar

--- app/src/main/java/com/tsquaredapplications/liquid/MainActivity.kt
+++ app/src/main/java/com/tsquaredapplications/liquid/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.tsquaredapplications.liquid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.hostFragment)
+        setupWithNavController(bottomNavigationView, navController)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.hostFragment).navigateUp()
 }
---
+++ app/src/main/java/com/tsquaredapplications/liquid/PresetsFragment.kt
@@ -0,0 +1,22 @@
+package com.tsquaredapplications.liquid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PresetsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_presets, container, false)
+    }
+}
