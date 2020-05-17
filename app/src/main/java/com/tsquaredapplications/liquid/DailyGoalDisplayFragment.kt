d109ce49550d1ccb83f1283afc9c34c895e85878
Tyler Turnbull <tylerturnbull13@gmail.com>
2020-05-17 11:22:58 -0400

    LIQ-47: UI for daily goal screen

--- app/src/main/java/com/tsquaredapplications/liquid/DailyGoalDisplayFragment.kt
+++ app/src/main/java/com/tsquaredapplications/liquid/DailyGoalDisplayFragment.kt
@@ -1,22 +1,15 @@
 package com.tsquaredapplications.liquid
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import com.tsquaredapplications.liquid.common.BaseFragment
+import com.tsquaredapplications.liquid.databinding.FragmentDailyGoalDisplayBinding
 
-/**
- * A simple [Fragment] subclass.
- */
-class DailyGoalDisplayFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_goal_display, container, false)
-    }
+class DailyGoalDisplayFragment : BaseFragment<FragmentDailyGoalDisplayBinding>() {
+    override fun setBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentDailyGoalDisplayBinding =
+        FragmentDailyGoalDisplayBinding.inflate(inflater, container, false)
 
 }
